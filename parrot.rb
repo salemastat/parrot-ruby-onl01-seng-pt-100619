46e411e3c1ca7a19d503576a9373eccda085b9f0
Salem Asrat <salemasrat@gmail.com>
2019-10-10 18:22:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(string = "Squawk!")
-"#{string}"
- end 
-
-parrot
+def parrot(string="Squawk!")
+  puts "#{string}"
+  "#{string}"
+end
